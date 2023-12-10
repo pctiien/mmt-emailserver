ab083c8df10e2522d9d9090b636b364efc58d2c0
pctien <tienpc>
2023-12-10 23:05:46 +0700

    nothing change

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,14 @@
 from socket import *
 import config
 import os
+from tkinter import *
+
+
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+
 
 bufferSize = 2048
 client_socket = socket(AF_INET, SOCK_STREAM)  
@@ -46,36 +54,43 @@ def send_mail(subject,body, from_addr, toEmail,ccEmail,bccEmail):
     send_msg(f"{email_header}", expect_return_msg=False)
     send_msg(f"Subject: {subject}\r\n{body}\r\n.", expect_return_msg=False)
  
-def send_txt_mail(subject, body, from_addr, toEmail, ccEmail, bccEmail, file_path):
-    # Đọc nội dung file txt
-    with open(file_path, "r") as file:
-        file_content = file.read()
+# gui file
+def send_file(subject, body, from_addr, toEmail, ccEmail, bccEmail, attachment_path=None):
+    msg = MIMEMultipart()
+    msg["From"] = from_addr
+    msg["To"] = ",".join(toEmail)
+    msg["Subject"] = subject
 
-    email_header = f"From: {from_addr}\r\n"
-    email_header += f"To: {','.join(toEmail)}\r\n"
     if len(ccEmail) > 0:
-        email_header += f"Cc: {','.join(ccEmail)}\r\n"
+        msg["Cc"] = ",".join(ccEmail)
 
     if len(bccEmail) > 0:
-        email_header += f"Bcc: {','.join(bccEmail)}\r\n"
+        print("kakakakaa")
+        msg["Bcc"] = ",".join(bccEmail)
+
+    # Thêm nội dung email
+    msg.attach(MIMEText(body, 'plain'))
+
+    if attachment_path:
+        attachment = MIMEBase('application', 'octet-stream')
+        with open(attachment_path, 'rb') as attachment_file:
+            attachment.set_payload(attachment_file.read())
+        encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', f'attachment; filename="{attachment_path}"')
+        msg.attach(attachment)
 
     send_msg(f"MAIL FROM:<{from_addr}>")
-    
+
     if len(toEmail + ccEmail) > 0:
         for mail in toEmail + ccEmail:
             send_msg(f"RCPT TO:<{mail}>")
 
-        send_msg("DATA")
-        send_msg(f"{email_header}\r\nSubject: {subject}\r\n{body}\r\n{file_content}\r\n.", expect_return_msg=False)
-  # if len(bccEmail)>0 :
-  #   for mail in bccEmail: 
-  #     header  = email_header + f"Bcc: {mail}\r\n"
-  #     ehlo()
-  #     send_msg(f"RCPT TO:<{mail}>")
-  #     send_msg(f"DATA")
-  #     send_msg(f"Subject: {subject}\r\n", expect_return_msg=False)
-  #     send_msg(f"{header}\r\n{body}\r\n.", expect_return_msg=False)
+    send_msg("DATA")
 
+    send_msg(msg.as_string(), expect_return_msg=False)
+
+    send_msg(".", expect_return_msg=False)
+  
 def create_folder(folder_name):
   try:
     os.makedirs(folder_name)
