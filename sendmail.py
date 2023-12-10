967af61e8fead754fe93cf59234ab300884a6fe1
Jurch13 <117813928+DucToan137@users.noreply.github.com>
2023-12-10 22:15:36 +0700

    sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,12 @@
 from socket import *
 import config
 import os
+from tkinter import *
+import base64
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 
 bufferSize = 2048
 client_socket = socket(AF_INET, SOCK_STREAM)  
@@ -48,6 +54,43 @@ def send_mail(subject,body, from_addr, toEmail,ccEmail,bccEmail):
   #     send_msg(f"Subject: {subject}\r\n", expect_return_msg=False)
   #     send_msg(f"{header}\r\n{body}\r\n.", expect_return_msg=False)
 
+# gui file
+def send_file(subject, body, from_addr, toEmail, ccEmail, bccEmail, attachment_path=None):
+    msg = MIMEMultipart()
+    msg["From"] = from_addr
+    msg["To"] = ",".join(toEmail)
+    msg["Subject"] = subject
+
+    if len(ccEmail) > 0:
+        msg["Cc"] = ",".join(ccEmail)
+
+    if len(bccEmail) > 0:
+        print("kakakakaa")
+        msg["Bcc"] = ",".join(bccEmail)
+
+    # Thêm nội dung email
+    msg.attach(MIMEText(body, 'plain'))
+
+    if attachment_path:
+        attachment = MIMEBase('application', 'octet-stream')
+        with open(attachment_path, 'rb') as attachment_file:
+            attachment.set_payload(attachment_file.read())
+        encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', f'attachment; filename="{attachment_path}"')
+        msg.attach(attachment)
+
+    send_msg(f"MAIL FROM:<{from_addr}>")
+
+    if len(toEmail + ccEmail) > 0:
+        for mail in toEmail + ccEmail:
+            send_msg(f"RCPT TO:<{mail}>")
+
+    send_msg("DATA")
+
+    send_msg(msg.as_string(), expect_return_msg=False)
+
+    send_msg(".", expect_return_msg=False)
+  
 def create_folder(folder_name):
   try:
     os.makedirs(folder_name)
