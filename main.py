d0d4635ad501a8e808d3bff2b56d57c20e3e1c82
Jurch13 <117813928+DucToan137@users.noreply.github.com>
2023-12-10 22:17:09 +0700

    main.py

--- main.py
+++ main.py
@@ -22,7 +22,16 @@ while(choice != '3'):
         bccEmail = [email.strip() for email in bccEmail.split(',')]
         email_subject = input("Subject :")
         email_msg = input("Content :")
-        sending.send_mail(subject=email_subject,body=email_msg,from_addr=config.username,toEmail=toEmail,ccEmail=ccEmail,bccEmail=bccEmail)
+        
+        fileEmail = input("Co gui kem file(1.co, 2.khong): ")
+        if(fileEmail == '1'):
+            slFile = int(input("So luong file muon gui: "))
+            for i in range(1,slFile+1):
+                b = input(f"Cho biet duong dan thu {i} ")
+            sending.send_txt(subject=email_subject,body=email_msg,from_addr=config.username,toEmail=toEmail,ccEmail=ccEmail,bccEmail=bccEmail,attachment_path=b)
+        elif(fileEmail == '2'):
+            sending.send_mail(subject=email_subject,body=email_msg,from_addr=config.username,toEmail=toEmail,ccEmail=ccEmail,bccEmail=bccEmail)
+            
     elif(choice == '2'):
         email_view = input("Email view :")
         getting.login(username=email_view)
